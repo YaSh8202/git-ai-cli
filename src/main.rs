17faa70808a61a987d2d6342b82b6b9a30506bd9
Yash <bajajy82@gmail.com>
2025-02-02 22:55:11 +0530

    Add dependencies and implement CLI structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{command, Parser, Subcommand, };
+
+
+
+#[derive(Parser)]
+#[command(name="git-ai")]
+#[command(about="AI Powered Cli tool for git commits")]
+struct Cli{
+    #[command(subcommand)]
+    command: Commands
+}
+
+
+#[derive(Subcommand)]
+enum Commands{
+    Generate{
+        #[arg(long)]
+        staged: bool
+    },
+    Explain{
+        #[arg(long)]
+        staged: bool,
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    match  cli.command {
+        Commands::Generate { staged } => {
+            println!("Generate command");
+            println!("Staged: {}", staged);
+        }
+        Commands::Explain { staged } => {
+            println!("Explain command");
+            println!("Staged: {}", staged);
+        }
+    }
 }
